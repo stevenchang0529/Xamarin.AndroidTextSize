a90bfae59801101f6701b66b48b8e7804d444167
Steven <steven@MSI-Steven>
2018-12-24 19:03:34 +0800

    加入專案檔案。

---
+++ AppGTest/AppGTest.Android/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Util;
+using Android.Content.Res;
+
+namespace AppGTest.Droid
+{
+    [Activity(Label = "AppGTest", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+
+            Configuration configuration = Resources.Configuration;
+            configuration.FontScale = 1f;
+            //0.85 小字型, 1 標準大小, 1.15 大字型，1.3 有點大字型 ，1.45超級大 
+            DisplayMetrics metrics = new DisplayMetrics();
+            this.WindowManager.DefaultDisplay.GetMetrics(metrics);
+            metrics.ScaledDensity = configuration.FontScale * metrics.Density;
+            this.BaseContext.Resources.UpdateConfiguration(configuration, metrics);
+
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ AppGTest/AppGTest.Android/MyLabelRenderer.cs
@@ -0,0 +1,21 @@
+﻿using Android.Content;
+using AppGTest.Droid;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+//[assembly: ExportRenderer(typeof(Label), typeof(MyLabelRenderer))]
+namespace AppGTest.Droid
+{
+    public class MyLabelRenderer : LabelRenderer
+    {
+        public MyLabelRenderer(Context context) : base(context)  { }
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            //抓取控制項上的字形設定，並重新設定原本的單位為DP
+            if (this.Control != null && this.Element != null)
+                this.Control.SetTextSize(Android.Util.ComplexUnitType.Dip, Convert.ToSingle(this.Element.FontSize));
+        }
+    }
+}
